89da3d2b45e118b884c931e8716f260867a180c6
Babis Stylianopoulos <chasty@chalmers.se>
2018-11-01 10:00:45 +0100

    Moved the backoff to 50% and increased the epoch and slot durations. You might want to put them back

--- apps/crystal/make_all_crystal_26_nodes.sh
+++ apps/crystal/make_all_crystal_26_nodes.sh
@@ -7,21 +7,23 @@
 export CFLAGS="$CFLAGS -DCOOJA=1"
 export CFLAGS="$CFLAGS -DMY_APP=1"
 export CFLAGS="$CFLAGS -DDISABLE_ETIMER=1"
-export CFLAGS="$CFLAGS -DBACKOFF=80"
+export CFLAGS="$CFLAGS -DBACKOFF=50"
+export CFLAGS="$CFLAGS -DAGGREGATE=1"
+export CFLAGS="$CFLAGS -DID_BUFFER_SIZE=1"
 
 
-export CFLAGS="$CFLAGS -DCRYSTAL_CONF_PERIOD=1"
+export CFLAGS="$CFLAGS -DCRYSTAL_CONF_PERIOD=1.5"
 export CFLAGS="$CFLAGS -DCRYSTAL_LOGGING=1"
 export CFLAGS="$CFLAGS -DDISABLE_UART=0"
 export CFLAGS="$CFLAGS -DCRYSTAL_SINK_ID=1" 
 export CFLAGS="$CFLAGS -DSTART_EPOCH=1" 
 export CFLAGS="$CFLAGS -DN_FULL_EPOCHS=1"
 
-export CFLAGS="$CFLAGS -DTX_POWER=31 -DRF_CHANNEL=26 -DCONCURRENT_TXS=5 -DNUM_ACTIVE_EPOCHS=1 -DN_TX_S=3 -DN_TX_T=3 -DN_TX_A=3 -DDUR_S_MS=4 -DDUR_T_MS=4 -DDUR_A_MS=4 -DCRYSTAL_LONGSKIP=0 -DCRYSTAL_SYNC_ACKS=1"
+export CFLAGS="$CFLAGS -DTX_POWER=31 -DRF_CHANNEL=26 -DCONCURRENT_TXS=5 -DNUM_ACTIVE_EPOCHS=1 -DN_TX_S=3 -DN_TX_T=3 -DN_TX_A=3 -DDUR_S_MS=8 -DDUR_T_MS=12 -DDUR_A_MS=12 -DCRYSTAL_LONGSKIP=0 -DCRYSTAL_SYNC_ACKS=1"
 export CFLAGS="$CFLAGS -DCRYSTAL_SINK_MAX_EMPTY_TS=5 -DCRYSTAL_MAX_SILENT_TAS=2 -DCRYSTAL_MAX_MISSING_ACKS=4 -DCRYSTAL_SINK_MAX_NOISY_TS=6"
 export CFLAGS="$CFLAGS -DCRYSTAL_USE_DYNAMIC_NEMPTY=0"
 export CFLAGS="$CFLAGS -DCCA_THRESHOLD=-32 -DCCA_COUNTER_THRESHOLD=80"
-export CFLAGS="$CFLAGS -DCRYSTAL_PAYLOAD_LENGTH=8"
+export CFLAGS="$CFLAGS -DCRYSTAL_PAYLOAD_LENGTH=25"
 #export CFLAGS="$CFLAGS -DSHORT_LOGS=1"
 
 #export CFLAGS="$CFLAGS -DCHHOP_MAPPING=CHMAP_nohop"
@@ -38,7 +40,7 @@ cp sndtbl_cooja.c sndtbl.c
 make clean
 rm crystal.sky
 
-method="gm naive"
+method="naive"
 timeout="2322580645" #20 hours but shrunk to 1 sec per measurement
 for meth in $method
 do
@@ -59,10 +61,10 @@ do
      do
          export CFLAGS="$CFLAGS -DSEPARATE_HEADERS=1"
          data_header="/media/sf_geometric_monitoring_shared/geometric-monitoring/contiki_project/intel_temp_headers/all_the_data_intel_node_"$node".h"
-         echo $data_header
+         #echo $data_header
          export CFLAGS="$CFLAGS -DSPECIFIC_HEADER=$data_header"
          #CFLAGS+= -DSPECIFIC_HEADER=$(data_header)
-         echo $CFLAGS
+         #echo $CFLAGS
          #exit 1
          make clean
          if [ $meth == "naive" ]
