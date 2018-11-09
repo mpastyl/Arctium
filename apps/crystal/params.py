58d54ade648e831f4896432ac0811c45ea2b1e7b
Babis Stylianopoulos <chasty@chalmers.se>
2018-11-09 16:20:10 +0100

    Converged to a good set of configs

--- apps/crystal/params.py
+++ apps/crystal/params.py
@@ -5,14 +5,14 @@ D_params ={
 "N_TX_S": [3],
 "DUR_S_MS": [8],
 
-"N_TX_T": [1,2,3],
-"DUR_T_MS": [12],
+"N_TX_T": [2],
+"DUR_T_MS": [8],
 
-"N_TX_A": [1,2,3],
-"DUR_A_MS": [12],
+"N_TX_A": [2],
+"DUR_A_MS": [8],
 
 
-"CRYSTAL_SINK_MAX_EMPTY_TS" : [5],
+"CRYSTAL_SINK_MAX_EMPTY_TS" : [3],
 "CRYSTAL_MAX_SILENT_TAS" : [2],
 "CRYSTAL_MAX_MISSING_ACKS" : [4]
 }
